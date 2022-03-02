f3fd40aa20f8b178fd374d4e7a1dc85fb4453de4
jiawei92 <40050673+jiawei92@users.noreply.github.com>
2022-03-02 15:44:07 -0700

    Update util_coord.py

--- osm2gmns/utils/util_coord.py
+++ osm2gmns/utils/util_coord.py
@@ -20,6 +20,7 @@ except ImportError:
     import math as mathlib
     use_numpy = False
 
+import numpy as np
 
 K0 = 0.9996
 
@@ -28,8 +29,8 @@ E2 = E * E
 E3 = E2 * E
 E_P2 = E / (1.0 - E)
 
-# SQRT_E = mathlib.sqrt(1 - E)
-_E = (1 - mathlib.sqrt(1 - E)) / (1 + mathlib.sqrt(1 - E))
+SQRT_E = np.sqrt(1 - E)
+_E = (1 - SQRT_E) / (1 + SQRT_E)
 _E2 = _E * _E
 _E3 = _E2 * _E
 _E4 = _E3 * _E
