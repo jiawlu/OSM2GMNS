4fabd4b709e2b96840a4d6a74ecd0d0dfcca4531
xyluo <36498464+Xiangyongluo@users.noreply.github.com>
2024-02-04 17:28:49 -0700

    add pytest for unittest

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, February 4th 2024
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+
