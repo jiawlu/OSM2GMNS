d7858ec8658dec8209ec9d5084b027321f5cc989
Jiawei Lu <lujiaweiwk@gmail.com>
2024-09-25 23:13:28 -0500

    use python downloader

--- osm2gmns/__init__.py
+++ osm2gmns/__init__.py
@@ -5,6 +5,7 @@
 
 from osm2gmns.osm2gmns import initlib
 from osm2gmns.osm2gmns import getNetFromFile, generateNodeActivityInfo, fillLinkAttributesWithDefaultValues, consolidateComplexIntersections, outputNetToCSV
+from osm2gmns.downloader import downloadOSMData
 
 __version__ = '1.0.0b3'
 print(__version__)
