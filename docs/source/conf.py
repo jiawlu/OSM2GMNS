49e904023d0ae1ac98e9880786519faab6c546b5
Jiawei Lu <lujiaweiwk@gmail.com>
2025-02-09 12:21:28 -0600

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../osm2gmns'))
+# sys.path.insert(0, os.path.abspath('../../osm2gmns'))
 
 autodoc_mock_imports = [
     "shapely",
