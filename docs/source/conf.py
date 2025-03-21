62151eb1b1b6f820ccfa52e00814d14f33493828
Jiawei Lu <lujiaweiwk@gmail.com>
2025-03-20 21:45:23 -0500

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-autodoc_mock_imports = [
-    "shapely",
-    "osmium",
-    "numpy"
-]
+# autodoc_mock_imports = [
+#     "shapely",
+#     "osmium",
+#     "numpy"
+# ]
 # -- Project information -----------------------------------------------------
 
 project = 'osm2gmns'
@@ -34,10 +34,11 @@ release = '1.0.0b5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', "sphinx.ext.napoleon"]
+# extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', "sphinx.ext.napoleon"]
+extensions = ['sphinx.ext.autodoc', "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -57,4 +58,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
