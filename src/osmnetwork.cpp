36613b8dea4abf8870a73e558e4c37db91c36921
Jiawei Lu <lujiaweiwk@gmail.com>
2025-04-15 22:38:56 -0500

    manage pool

--- src/osmnetwork.cpp
+++ src/osmnetwork.cpp
@@ -32,6 +32,7 @@
 #include <osmium/osm/relation.hpp>
 #include <osmium/osm/tag.hpp>
 #include <osmium/osm/way.hpp>
+#include <osmium/thread/pool.hpp>
 #include <osmium/visitor.hpp>  // NOLINT
 #include <regex>
 #include <string>
@@ -552,38 +553,41 @@ OsmNetwork::OsmNetwork(const std::filesystem::path& osm_filepath, absl::flat_has
 
   const auto time1 = std::chrono::high_resolution_clock::now();
   OsmHandler handler(mode_types_, link_types_, connector_link_types_, POI_, osm_parsing_config_);
+  osmium::thread::Pool pool {};
   try {
     const osmium::io::File input_file{osm_filepath.string()};
 
-    {
-      osmium::io::Reader reader{input_file};
-      const osmium::Box& box = reader.header().box();
-      if (reader.header().box().valid()) {
-        boundary_ = factory_->createPolygon({geos::geom::Coordinate(box.bottom_left().lon(), box.bottom_left().lat()),
-                                             geos::geom::Coordinate(box.top_right().lon(), box.bottom_left().lat()),
-                                             geos::geom::Coordinate(box.top_right().lon(), box.top_right().lat()),
-                                             geos::geom::Coordinate(box.bottom_left().lon(), box.top_right().lat()),
-                                             geos::geom::Coordinate(box.bottom_left().lon(), box.bottom_left().lat())});
-      } else {
-        LOG(INFO) << "no valid boundary information in the osm file";
-      }
+    // {
+    //   osmium::io::Reader reader{input_file};
+    //   const osmium::Box& box = reader.header().box();
+    //   if (reader.header().box().valid()) {
+    //     boundary_ = factory_->createPolygon({geos::geom::Coordinate(box.bottom_left().lon(), box.bottom_left().lat()),
+    //                                          geos::geom::Coordinate(box.top_right().lon(), box.bottom_left().lat()),
+    //                                          geos::geom::Coordinate(box.top_right().lon(), box.top_right().lat()),
+    //                                          geos::geom::Coordinate(box.bottom_left().lon(), box.top_right().lat()),
+    //                                          geos::geom::Coordinate(box.bottom_left().lon(), box.bottom_left().lat())});
+    //   } else {
+    //     LOG(INFO) << "no valid boundary information in the osm file";
+    //   }
 
-      if (POI_) {
-        handler.updateParseTargets(false, false, true);
-        osmium::apply(reader, handler);
-      }
-      reader.close();
-    }
+    //   if (POI_) {
+    //     handler.updateParseTargets(false, false, true);
+    //     osmium::apply(reader, handler);
+    //   }
+    //   reader.close();
+    // }
 
     {
-      osmium::io::Reader reader_way{input_file};
+      // osmium::io::Reader reader_way{input_file};
+      osmium::io::Reader reader_way{input_file, osmium::osm_entity_bits::way, pool}; // Pass pool
       handler.updateParseTargets(false, true, false);
       osmium::apply(reader_way, handler);
       reader_way.close();
     }
 
     {
-      osmium::io::Reader reader_node{input_file};
+      // osmium::io::Reader reader_node{input_file};
+      osmium::io::Reader reader_node{input_file, osmium::osm_entity_bits::node, pool}; // Pass pool
       handler.updateParseTargets(true, false, false);
       osmium::apply(reader_node, handler);
       reader_node.close();
